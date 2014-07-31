4c1b4f338a13fa9058a641b5a1b8c4dd42315808
Michael Tseng <matseng@gmail.com>
2014-07-31 00:10:17 -0700

    Added karma config file and basic tests

---
+++ tests/appSpec.js
@@ -0,0 +1,8 @@
+define(['myLibrary'], function(myLibrary) {
+
+  describe('myLibrary modules', function() {
+    it('should be able to add, subtract, multiply and divide', function() {
+      expect(myLibrary.add(1, 2)).toBe(3);
+    });
+  });
+});
---
+++ tests/test-main.js
@@ -0,0 +1,21 @@
+// This creates an array of all the files that Karma finds with a suffix of
+// Spec.js (eg utilsSpec.js) to be added to the Require JS config below
+var tests = [],
+    file;
+for (file in window.__karma__.files) {
+    if (window.__karma__.files.hasOwnProperty(file)) {
+        if(/Spec\.js$/.test(file)) {
+            tests.push(file);
+        }
+    }
+}
+requirejs.config({
+    baseUrl: '/base/myLibrary',  // Karma serves files from /base/<your-base-path>
+    paths: {
+        //.. the paths you have set in your Require Js main.js file
+        // eg jquery: "libs/jquery.min"
+    },
+    deps: tests,  // add tests array to load our tests
+
+    callback: window.__karma__.start  // start tests once Require.js is done
+});
